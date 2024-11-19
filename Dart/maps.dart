47f1293187f281e309ec9bb5d0c8d33707ad3837
Maaxboon <maaxwelokoth@gmail.com>
2024-11-19 12:38:22 +0300

    Adds maps in dart

---
+++ Dart/maps.dart
@@ -0,0 +1,26 @@
+// A map is a dynamic collection that represents a set of values ​as key-value pairs. Keys and values ​in the
+
+// map can be of any type.
+
+// Student ages
+void main(){
+    // Creating a Map with string keys and int values
+    Map<String, int> ages = {
+        'Alice' : 30,
+        'Bob' : 25,
+        'Charlie' : 35,
+    };
+
+    // Players nationalities
+    Map<String, String> Nationalities = {
+    'Messi': 'Argentina',
+    'Ronaldo' : 'Portugal',
+    'Pele' : 'Brazil',
+    'Joshua' : 'Germany',
+    };
+    print("Ages of students: $ages");
+    print("Nationalities of players: $Nationalities");
+}
+
+
+
