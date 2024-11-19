6a69e59b116c7422c9666b995b26704979144f79
Maaxboon <maaxwelokoth@gmail.com>
2024-11-19 12:19:44 +0300

    Adds booleans & decision making

---
+++ Dart/booleans.dart
@@ -0,0 +1,18 @@
+void main(){
+    // Boolean code: True or false logic
+    bool isCodingFun = true;
+    bool lovesStalling = false; // A commit a day to keep the machine running
+
+    // Making decisions with booleans
+    if (isCodingFun){
+        print("Coding is fun, keep going!");
+    } else{
+        print("Try new language or project for more fun!");
+    }
+
+    if (lovesStalling){
+        print("This should be worked on to build on consistency!");
+    } else {
+        print("Way to go, keep consistency!");
+    }
+}
