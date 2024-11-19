5dd8ac2892a9cec05bd33595706258c970320091
Maaxboon <maaxwelokoth@gmail.com>
2024-11-19 10:08:37 +0300

    Adds sets

---
+++ Python/sets.py
@@ -0,0 +1,6 @@
+#  Set is an unordered collection of unique items. Defined by values separated by commas inside braces
+
+student_ids = {112, 114, 117, 113}
+print(student_ids)
+
+# Since sets are unordered collections, indexing has no meaning. Hence, the slicing operator [] does not work.
