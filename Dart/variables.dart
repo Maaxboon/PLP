e0add9b9a919e4dd568b5b8ddaf78a7da827f1c5
Maaxboon <maaxwelokoth@gmail.com>
2024-11-19 11:41:29 +0300

    Understanding variables in Dart

---
+++ Dart/variables.dart
@@ -0,0 +1,15 @@
+void main(){
+    String wizardName = 'Gandalf'; // A string spell
+    int magicLevel = 100; // Integer magic
+    double spellPower = 75.5; // Double trouble
+    bool hasMagicStaff = true; // A magical staff? True or false?
+
+    print('Name: $wizardName');
+    print('Magic Level: $magicLevel');
+    print('Spell power: $spellPower');
+    print('Has staff: $hasMagicStaff');
+
+    // Update variable
+    magicLevel = 110;
+    print('New Magic Level: $magicLevel');
+}
