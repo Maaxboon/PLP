2db179b5baa950b4d90dd13af10a86fab4c60207
Maaxboon <maaxwelokoth@gmail.com>
2024-11-19 13:21:55 +0300

    Adds dart control flow

---
+++ Dart/control-flow.dart
@@ -0,0 +1,12 @@
+// if statement: this simple decision-make runs a block of code if a condition is true
+void main(){
+    var age = 16;
+
+    if (age > 16) {
+        print("Kyle is eligible to start driving classes in Kenya");
+    } else if (age == 16) {
+        print("Kyle is old enough to start driving in Kenya!");
+    }else{
+        print("Kyle is too young to start driving in Kenya");
+    }
+}
