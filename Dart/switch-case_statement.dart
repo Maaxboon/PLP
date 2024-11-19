72881c1d42e20730451acc2aeb2f926948ee7bda
Maaxboon <maaxwelokoth@gmail.com>
2024-11-19 13:27:29 +0300

    Adds dart switch case statement

---
+++ Dart/switch-case_statement.dart
@@ -0,0 +1,19 @@
+// In Dart, the switch statement is used to evaluate an expression and execute different blocks of code based on matching cases. 
+
+// Think of switch as a more organized if-else—
+
+void main() {
+    int day = 3;
+
+    switch (day) {
+        case 1:
+        print("Monday: Let's code!");
+        case 2: 
+        print("Tuesday: Bless my gallery!");
+        case 3: 
+        print("Wednesday: We are halfway!");
+        break;
+        default:
+        print("Time for the weekend!");
+    }
+}
