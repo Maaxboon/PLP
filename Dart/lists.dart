d331ce73e2204e121aaec291bd005ef4643f3f5c
Maaxboon <maaxwelokoth@gmail.com>
2024-11-19 12:25:17 +0300

    Adds lists in dart

---
+++ Dart/lists.dart
@@ -0,0 +1,13 @@
+void main(){
+    // Use square brackets for listing
+    List myList = [1, 2, 3, 7, "Maaxboon"];
+
+    // Adding a value to the list
+    myList.add(67);
+
+    // Removing a value
+    myList.remove("Maaxboon");
+
+    print(myList);
+
+}
