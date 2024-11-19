cf9aa0afc775c376a1b6b9f7da2aa841c58929ec
Maaxboon <maaxwelokoth@gmail.com>
2024-11-19 10:08:59 +0300

    Adds variables

---
+++ Python/variables.py
@@ -0,0 +1,41 @@
+# In programming, a variable is a container (storage area) to hold data. For example,
+
+number = 10
+
+# Here, the number is the variable storing the value 10.
+
+# Assigning values to Variables in Python
+
+# As we can see from the above example, we use the assignment operator = to assign a value to a variable.
+
+site_name = "Power Learn Project"
+print(site_name)
+
+# Assigning values to Variables in Python
+
+# As we can see from the above example, we use the assignment operator = to assign a value to a variable.
+
+# Changing the Value of a Variable in Python
+
+site_name = "Power Learn Project"
+print(site_name)
+
+# Assigning new value to site_name
+site_name = "ALU"
+print(site_name)
+
+# Here, the value of site_name is changed from ‘Power Learn Project’ to 'ALU'.
+
+# Example: Assigning multiple values to multiple variables
+a, b, c = 5, 7, "Nairobi"
+print(a) # print 5
+print(b) # prints 7
+print(c) # prints Nairobi
+
+# Rules for naming variables
+# snake_case
+# MACRO_CASE
+# camelCase
+# CapWords
+# Avoid using keywords
+
