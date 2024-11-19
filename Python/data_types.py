09cc693cb339239da1de56f936d82d407f730da4
Maaxboon <maaxwelokoth@gmail.com>
2024-11-19 10:08:12 +0300

    Adds dictionaries

---
+++ Python/data_types.py
@@ -0,0 +1,29 @@
+# Data Types
+# Numeric data types
+# integers: int: 1, 39, 300
+# float: decimal numbers: 3.5
+
+# List
+# An ordered collection of similar or different types of items separated by commas and enclosed within brackets
+languages = ['Python', 'Dart', 'JavaScript']
+print(languages)
+
+# Accessing list items
+# To access list items we use the index number (0, 1, 2, ....)
+
+languages = ['Python', 'Dart', 'JavaScript', 'Java', 'Solidity']
+print(languages[-1])
+
+# Tuples
+# An ordered sequence of items same as a list. The difference is that tuples are immutable.
+# We use parentheses to store values in a tuple
+
+products = ('PES', 'XBOX', 'FIFA', 'MORTAL KOMBAT')
+print(products)
+
+# Access Tuple Items
+
+# Similar to lists, we use the index number to access tuple items
+products = ('PES', 'XBOX', 'FIFA', 'MORTAL KOMBAT')
+print(products[2])
+
