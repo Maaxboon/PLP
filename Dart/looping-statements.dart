6ef58963faf60e22e13196e1f8bc7b3cfe960ce6
Maaxboon <maaxwelokoth@gmail.com>
2024-11-19 13:32:43 +0300

    Adds dart for loop

---
+++ Dart/looping-statements.dart
@@ -0,0 +1,14 @@
+// Dart Loop is used to run a block of code repetitively for a given number of times or until matches the specified condition. Loops are essential tools for any programming language. 
+
+// Loops let you repeat a block of code multiple times! They are like running on a treadmill until you reach your goal. 🏃‍♂️
+
+// Dart for loop
+
+// The for loop is used when we know how many times a block of code will execute.
+
+// When you know exactly how many times you want to repeat something, use a for loop.
+void main() {
+    for (int i = 1; i <= 5; i++) {
+        print("This is loop iteration $i");
+    }
+}
